67c0ed9d679a24d0001cfca637b0ffce47614217
xennon <gameoveralisa@gmail.com>
2024-04-10 16:43:15 +0800

    Init files

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+
+export default function Login() {
+  const modalRef = useRef<any>()
+
+
+  return (
+    <div className="flex flex-col items-center m-auto w-100 bg-white glass p-5 sm:w-[300px] md:w-[40%] xl:w-[30%] h-[100%] rounded-xl ">
+      <div className="title font-bold text-xl">NR Dashboard </div>
+    <div className="">This page is will be available soon!</div>
+    </div>
+
+  );
+}

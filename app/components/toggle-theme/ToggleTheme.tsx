806de1c58498f0f437bc1eda9d099e6b00100234
xennon <gameoveralisa@gmail.com>
2024-04-17 13:02:23 +0800

    Fixed erros in render component & Updated toggle theme icon display

--- app/components/toggle-theme/ToggleTheme.tsx
+++ app/components/toggle-theme/ToggleTheme.tsx
@@ -30,11 +30,11 @@ const ToggleTheme = () => {
         <div className="relative items-center justify-center flex">
           <input onChange={handleToggle} defaultChecked={checkTheme()} type="checkbox" id="toggle-switch" className="peer relative shrink-0 w-11 h-6 scale-150 p-px bg-gray-100 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-whsite disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-gray-100 checked:border-white focus:checked:border-white dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-white dark:checked:border-white dark:focus:ring-offset-gray-600 focus:border-none focus:shadow-none before:inline-block before:size-5 before:bg-white checked:before:bg-black-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-neutral-400 dark:checked:before:bg-white" />
           <label htmlFor="toggle-switch" className="sr-only">switch</label>
-          <label role="button" htmlFor="toggle-switch" className="hidden peer-checked:block absolute top-0 right-[-3px]">
-            <MoonIcon className="h-6 w-6 text-black" />
-          </label>
-          <label role="button" htmlFor="toggle-switch" className="block peer-checked:hidden absolute top-0 left-[-4px]" >
+          <label role="button" htmlFor="toggle-switch" className="block peer-checked:hidden absolute top-0 right-[-3px]">
             <SunIcon className="h-6 w-6 text-black" />
+          </label>
+          <label role="button" htmlFor="toggle-switch" className="hidden peer-checked:block absolute top-0 left-[-4px]" >
+            <MoonIcon className="h-6 w-6 text-black" />
           </label>
         </div>
 
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,28 +1,36 @@
 'use client'
+import { useEffect, useState } from "react";
 import Image from "next/image"
 import Input from "@/app/components/forms/Input";
 import Button from "@/app/components/forms/Button";
 import NRLogo from "@/public/nr.png"
 import Link from "@/app/components/navs/Link";
 export default function Login() {
+  const [date, setDate] = useState<Date>()
 
   const displayDate = () => {
-    return new Date().toLocaleString();
+    return date?.toLocaleString();
   }
 
   const getCurrentYear = () => {
-    return new Date().getFullYear()
+    return date?.getFullYear()
   }
+  useEffect(() => {
+    setInterval(() => { setDate(new Date) }, 1000)
+  }, [])
+
   return (
     <div className="
-    p-10
+    p-10 fadeIn
     dark:bg-gray-800 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600
     flex flex-col m-auto w-100 items-center bg-white glass sm:w-[300px] md:w-[40%] xl:w-[400px] h-[100%] rounded-xl ">
       <div className="">
         <Image className="object-cover w-[100px] h-[100px] opacity-90" src={NRLogo} alt="" />
       </div>
       <div className="title font-bold text-2xl dark:text-white">NR Login </div>
-      <div className="text-[13px]">{displayDate()}</div>
+      {date ? <div className="text-[13px] fadeIn">{displayDate() || "Hi, Welcome!"}</div> :
+        <div>Welcome!</div>
+      }
 
       <Input
         wrapperClassName="mt-5"
@@ -57,7 +65,7 @@ export default function Login() {
       </Link>
 
       <div className="mt-4 text-[14px] text-center">
-     Copyright © {getCurrentYear()} <span className="">NR Realty Development</span>
+        Copyright © {getCurrentYear()} <span className="">NR Realty Development</span>
       </div>
     </div>
 
